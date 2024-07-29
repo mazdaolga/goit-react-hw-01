41ba1a5ffbcac0cb1a30b6fc612f754b17ae45d4
Olga Ovcharenko <157131948+mazdaolga@users.noreply.github.com>
2024-07-29 16:10:20 +0300

    my work

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./App.css"
+import Profile from './components/Profile';
+import FriendList from './components/FriendList';
+import TransactionHistory from './components/TransactionHistory';
+import userData from './data/userData.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  );
+};
+
+export default App;
+
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import FriendListItem from './FriendListItem';
+import styles from '../components/FriendList.module.css';
+
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => (
+        <li key={friend.id} className={styles.item}>
+          <FriendListItem
+            avatar={friend.avatar}
+            name={friend.name}
+            isOnline={friend.isOnline}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default FriendList;
